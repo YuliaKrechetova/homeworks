2770ff8da65e47296dd61034e5534e1802b7c19b
ykrechet <yulia.krechet@gmail.com>
2016-07-07 15:20:50 +0300

    Ajax and temlate are working properly

--- products-by-types/script.js
+++ products-by-types/script.js
@@ -1,29 +1,45 @@
 $(document).ready(function(){
-
-	var html = _.template(tmpl)({
-	  title: "Сладости",
-	  items: [
-	    "Торт",
-	    "Печенье",
-	    "Пирожное"
-	  ]
-	});
 	
 	$.ajax({
 		url: 'products.json',
 		dataType: 'json',
 		contentType: 'application/json'
 	}).done(function(result) {
-		// var productsList = result;
-		// var onSale = productsList.filter(function(type) {
-		// 	return type == 'sale';			
-		// });
-		alert(result);
-		var msg = $("<p></p>");
-		msg.append(result.pic);
-		msg.append("Название товара: " + result.name + ";");
-		msg.append("Цена: " + result.price + "руб.");		
-		$('#sale').find('.products-container').html(msg).fadeIn();
+		if (result.success) {
+			var templateString = $('#menu-template').html(); // берем "шаблон" из index.html
+			var template = _.template(templateString);
+			var productsList = result.data;
+
+			// фильтр распродажи
+			var onSaleItems = productsList.filter(function(item) {
+				return item.type == 'sale';			
+			});
+			var filteredMassive = template({data: onSaleItems}); // поскольку в переменную template мы записали функцию, она тоже стала функцией и мы даем ей агрумент
+
+			$('#on-sale-container').html(filteredMassive); // отдаем содержимое переменной в нужный нам контейнер
+
+			// фильтр промо-акций
+			var promoItems = productsList.filter(function(item) {
+				return item.type == 'promo';			
+			});
+			var filteredMassive = template({data: promoItems}); 
+
+			$('#promo-container').html(filteredMassive); 
+
+			// фильтр рекомендуемых
+			var recommendedItems = productsList.filter(function(item) {
+				return item.type == 'recommended';			
+			});
+			var filteredMassive = template({data: recommendedItems}); 
+
+			$('#recommended-container').html(filteredMassive); 
+
+		} else {
+			alert(result.message);
+		}
+		
+		
+		console.log(result);
 	}).fail(function(request, errorType, errorMessage) {
 		alert('Error: ' + errorType + errorMessage);
 	}).always(function (xhr, status) {
